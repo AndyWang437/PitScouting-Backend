158bc98bf15a33dfcc30e35f80ac997370e3871c
Andy Wang <andywang.437@gmail.com>
2025-01-19 13:31:03 -0500

    Updated Dependencies

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,37 +1,36 @@
 require('dotenv').config();
-
 const parseDbUrl = (url) => {
-  if (!url) return {};
-  const matches = url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-  if (!matches) return {};
-  return {
-    username: matches[1],
-    password: matches[2],
-    host: matches[3],
-    port: matches[4],
-    database: matches[5],
-  };
+    if (!url)
+        return {};
+    const matches = url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+    if (!matches)
+        return {};
+    return {
+        username: matches[1],
+        password: matches[2],
+        host: matches[3],
+        port: matches[4],
+        database: matches[5],
+    };
 };
-
 const dbConfig = parseDbUrl(process.env.DATABASE_URL);
-
 module.exports = {
-  development: {
-    ...dbConfig,
-    dialect: 'postgres',
-  },
-  test: {
-    ...dbConfig,
-    dialect: 'postgres',
-  },
-  production: {
-    ...dbConfig,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+    development: {
+        ...dbConfig,
+        dialect: 'postgres',
     },
-  },
-}; 
+    test: {
+        ...dbConfig,
+        dialect: 'postgres',
+    },
+    production: {
+        ...dbConfig,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+};
